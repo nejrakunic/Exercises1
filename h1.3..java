9eec63cec5317e0439a49ea52f4845bd9fb881cd
nejrakunic <116256075+nejrakunic@users.noreply.github.com>
2022-10-20 12:17:05 +0200

    Add files via upload

---
+++ h1.3..java
@@ -0,0 +1,12 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+        int s=60;
+        int m=60;
+        int h=24;
+        int d=365;
+        int r;
+        r=d*h*m*s;
+        System.out.println("there are "+r+" seconds in a year");	}
+}
